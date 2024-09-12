ead88a35e2090c1dead54cbf458314e0d05d760e
loof <ypeissard@gmail.com>
2024-09-12 22:21:13 +0200

    Implements patch controller method for cart item.

--- src/main/java/ch/roester/cart/CartItemService.java
+++ src/main/java/ch/roester/cart/CartItemService.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 @Slf4j
@@ -23,13 +24,15 @@ public class CartItemService {
     private final CartItemMapper cartItemMapper;
     private final EventRepository eventRepository;
     private final VariantRepository variantRepository;
+    private final CartRepository cartRepository;
 
     @Autowired
-    public CartItemService(CartItemRepository cartItemRepository, CartItemMapper cartItemMapper, EventRepository eventRepository, VariantRepository variantRepository) {
+    public CartItemService(CartItemRepository cartItemRepository, CartItemMapper cartItemMapper, EventRepository eventRepository, VariantRepository variantRepository, CartRepository cartRepository) {
         this.cartItemMapper = cartItemMapper;
         this.cartItemRepository = cartItemRepository;
         this.eventRepository = eventRepository;
         this.variantRepository = variantRepository;
+        this.cartRepository = cartRepository;
     }
 
     public Page<CartItemResponseDTO> findAll(Pageable pageable) {
@@ -42,15 +45,29 @@ public class CartItemService {
     }
 
 
+    @Transactional
     public CartItemResponseDTO update(Integer id, CartItemRequestDTO updatingCartItem) {
         CartItem existingCartItem = cartItemRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        Variant variant = variantRepository.findById(updatingCartItem.getVariantId()).orElseThrow(EntityNotFoundException::new);
+        double deltaAmount = updatingCartItem.getAmount() - existingCartItem.getAmount();
+        Cart cart = cartRepository.findById(updatingCartItem.getCartId()).orElseThrow(EntityNotFoundException::new);
+        BigDecimal currentTotal = cart.getTotal();
+        cart.setTotal(currentTotal.add(variant.getStockMultiplier().multiply(variant.getProduct().getPricePerUnit()).multiply(BigDecimal.valueOf(deltaAmount))));
+        cartRepository.save(cart);
         BeanUtils.copyProperties(updatingCartItem, existingCartItem);
         return cartItemMapper.toResponseDTO(cartItemRepository.save(existingCartItem));
     }
 
+    @Transactional
     public CartItemResponseDTO save(CartItemRequestDTO cartItemRequestDTO) {
-        //Event event = eventRepository.findById(cartItemRequestDTO.getEventId()).orElseThrow(EntityNotFoundException::new);
         Variant variant = variantRepository.findById(cartItemRequestDTO.getVariantId()).orElseThrow(EntityNotFoundException::new);
+        Cart cart = cartRepository.findById(cartItemRequestDTO.getCartId()).orElseThrow(EntityNotFoundException::new);
+        if (cart.getTotal() != null) {
+            cart.setTotal(cart.getTotal().add(variant.getStockMultiplier().multiply(variant.getProduct().getPricePerUnit()).multiply(BigDecimal.valueOf(cartItemRequestDTO.getAmount())) ));
+        } else {
+            cart.setTotal(variant.getStockMultiplier().multiply(variant.getProduct().getPricePerUnit()).multiply(BigDecimal.valueOf(cartItemRequestDTO.getAmount())));
+        }
+        cartRepository.save(cart);
         CartItem cartItem = cartItemMapper.fromRequestDTO(cartItemRequestDTO);
         cartItem.setVariant(variant);
         return cartItemMapper.toResponseDTO(cartItemRepository.save(cartItem));
